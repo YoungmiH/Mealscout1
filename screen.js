c7c47280e5e7d01e02eb415ede1e755cbd1fab4b
YoungmiH <ahn.designer@gmail.com>
2019-09-11 15:51:58 -0700

    changes

--- screen.js
+++ screen.js
@@ -1,15 +1,7 @@
 var page =0;
 var video = document.querySelector("#videoElement");
 
-function preload(){
-}
 
-
-function setup(){
-
-  canvas = createCanvas(375,667);
-
-}
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
